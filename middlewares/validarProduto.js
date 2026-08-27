683a00247b3799583573162afe4aec331cbf7d82
magalhaesmedeirod-design <magalhaesmedeirod@gmail.com>
2026-08-27 14:24:00 -0400

    nmxchjnkgmbjhtjgnjx g

---
+++ middlewares/validarProduto.js
@@ -0,0 +1,29 @@
+const CAMPOS_PRODUTOS = ['nome', 'descricao', 'preco', 'quantidade_estoque', 'categorias_id'];
+
+export function validarProduto(req, res, next) {
+    const {nome, preco, quantidade_estoque, categorias_id} = req.body;
+    const erros = [];
+
+    if (!nome || typeof nome !== 'string' || !nome.trim()) {
+        erros.push('nome é obrigatório e deve ser um texto');
+    }
+
+    if (preco === undefined || preco === null || typeof preco !== 'number' || preco <=0) {
+        erros.push('preço é obrigatóri e deve ser um número maior que zero');
+    }
+
+    if (quantidade_estoque !== undefined && (typeof quantidade_estoque !== 'number' || quantidade_estoque < 0)) {
+        erros.push('quantidade_estoque deve ser um número maior ou igual a zero');
+    }
+
+    if (categorias_id || categorias_id !== undefined || categorias_id !== null || typeof categorias_id !== 'number') {
+        erros.push('categorias_id é obrigatório e deve ser um número')
+    }
+
+    if (erros.length > 0) {
+        return res.status(400).json({erros});
+    }
+
+    next();
+
+}
